ad542e3cfb56cb53696219fad4716e7a3870ec94
brockeetonCentralia <broc.keeton@student.centralia.edu>
2024-10-03 15:23:28 -0700

    Formating. Still need sleep and image background

--- scripts.js
+++ scripts.js
@@ -1,36 +1,41 @@
-const words = ["Nulla", "facilisi", "Cras sed odio sed leo laoreet molestie id non purus.."];
+const words = ["Broc Keeton.", "a Software Developer.", "a Learner."];
 let i = 0;
 let counter;
+// let pause;
+
 function typeNow() {
-   let word = words[i].split("");
-   var loopTyping = function() {
-      if (word.length > 0) {
-         document.getElementById('text').innerHTML += word.shift();
-      } else {
-         deleteNow();
-         return false;
-      };
-      counter = setTimeout(loopTyping, 200);
-   };
-   loopTyping();
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('text').innerHTML += word.shift();
+        } else {
+            // pause = setTimeout(400);
+            deleteNow();
+            return false;
+        };
+        counter = setTimeout(loopTyping, 100);
+        
+    };
+    loopTyping();
 };
+
 function deleteNow() {
-   let word = words[i].split("");
-   var loopDeleting = function() {
-      if (word.length > 0) {
-         word.pop();
-         document.getElementById('text').innerHTML = word.join("");
-      } else {
-         if (words.length > (i + 1)) {
-            i++;
-         } else {
-            i = 0;
-         };
-         typeNow();
-         return false;
-      };
-      counter = setTimeout(loopDeleting, 200);
-   };
-   loopDeleting();
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('text').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typeNow();
+            return false;
+        };
+        counter = setTimeout(loopDeleting, 100);
+    };
+    loopDeleting();
 };
 typeNow();
